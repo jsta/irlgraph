5fcca09b31628a51d0bb3a9138d3c48cb0939b83
jsta <jstachelek@utexas.edu>
2015-09-04 14:13:29 -0400

    finish vignette, streamline package

--- R/get_path.R
+++ R/get_path.R
@@ -1,18 +1,28 @@
-#'@name get_path
+#'@name acc_path
 #'@title Generate a shortest path raster from a specific graph node
 #'@export
 #'@importFrom igraph shortest.paths
 #'@param egraph2
 #'@param snode
+#'@param scoord
 #'@param csurf
 
-get_path <- function(egraph2, snode, csurf){
+acc_path <- function(graph, snode = NULL, scoord = NULL, costsurf){
   
   #ADD A CHECK TO MAKE SURE SNODE EXISTS IN GRID!!!
   
-  spaths <- igraph::shortest.paths(egraph2, v = snode)#171
+  if(is.null(snode) & all(is.null(scoord))){
+    stop("Must supply either a starting node or coordinates")
+  }
   
-  result <- as(csurf, "RasterLayer")
+  if(is.null(snode)){
+    snode <- raster::cellFromXY(costsurf, scoord)
+  }
+  
+  #snode <- 2
+  spaths <- igraph::shortest.paths(graph, v = snode)
+  
+  result <- as(costsurf, "RasterLayer")
   result[] <- NA
   result[] <- spaths
   result
